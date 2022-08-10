23356ed0309ad7a3fa174445b45c90075b1f8054
wdcunha@gmail.com <wdcunha@gmail.com>
2022-08-10 17:26:16 -0300

    Add total to table in the business list

--- src/app/components/business/business.component.ts
+++ src/app/components/business/business.component.ts
@@ -2,6 +2,7 @@ import { animate, state, style, transition, trigger } from '@angular/animations'
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { Business } from '../../models/business.model';
 import { BusinessProductService } from '../../services/business-product.service';
 import { BusinessService } from '../../services/business.service';
@@ -20,10 +21,10 @@ import { BusinessService } from '../../services/business.service';
 })
 export class BusinessComponent implements OnInit {
 
-  title: string | undefined;
-  entity: string | undefined;
-
   business$: Observable<Business[]> | undefined;
+  entity: string | undefined;
+  totalValue: number = 0;
+  title: string | undefined;
 
   displayedColumns = ['cod', 'provider', 'occurenceDate', 'businessAmount', 'payment', 'installments', 'value', 'notes'];
 
@@ -37,18 +38,21 @@ export class BusinessComponent implements OnInit {
     private route: ActivatedRoute) {}
 
     ngOnInit(): void {
-      this.route.queryParams.subscribe(params => {
-        this.business$ = this.purchasesService.getAllBusiness(params.type);
+      this.route.params.subscribe(params => {
+
+        let businessTypeId = 0;
 
         switch(params.type) {
-          case '1': {
+          case 'lista-compras': {
             this.title = "Compras"
             this.entity = "Fornecedor"
+            businessTypeId = 1;
             break;
           }
-          case '2': {
+          case 'lista-vendas': {
             this.title = "Vendas"
             this.entity = "Cliente"
+            businessTypeId = 2;
             break;
           }
           default: {
@@ -57,7 +61,27 @@ export class BusinessComponent implements OnInit {
             break;
           }
         }
+
+        this.business$ = this.purchasesService.getAllBusiness(businessTypeId);
+        this.calcTotal();
       });
+  }
+
+  calcTotal() {
+
+    let varVal;
+
+    const teste = this.business$?.forEach(x => x.map(bt => bt.businessTotal)
+    .reduce((acc, val) => acc! +val!, 0));
+
+    console.log(teste);
+
+    const totalPrice = this.business$?.pipe(
+      map(x => x),
+      map(bt => bt.map(y => y.businessTotal).reduce((acc, val) => acc! + val!, 0))
+    );
+
+    totalPrice?.subscribe(x => this.totalValue = x as number);
   }
 
 }
