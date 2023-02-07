1907b6509dd047ac206612db6612b97c8b293bab
wdcunha@gmail.com <wdcunha@gmail.com>
2023-02-07 09:15:31 -0300

    Adjustment to try solving ng not found in heroku

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
+function requireHTTPS(req, res, next) {
+  // The 'x-forwarded-proto' check is for Heroku
+  if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+      return res.redirect('https://' + req.get('host') + req.url);
+  }
+  next();
+}
+
 //Install express server
 const express = require('express');
-const path = require('path');
 
 const app = express();
 
+app.use(requireHTTPS);
 // Serve only the static files form the dist directory
 app.use(express.static('./dist/financecontrol-frontend'));
 
@@ -12,6 +20,6 @@ app.get('/*', (req, res) =>
 );
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 3000, function(){
+app.listen(process.env.PORT || 8080, function(){
   console.log("Node app is running at localhost:" + app.get('port'));
 });
